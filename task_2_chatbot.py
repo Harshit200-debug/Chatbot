ad496fabe7dcb982c65bb13c6ee474e274589209
Harshit_kushwah <harshit.kushwahji.21@gmail.com>
2024-07-15 12:15:11 +0530

    Add files via upload

---
+++ task_2_chatbot.py
@@ -0,0 +1,108 @@
+import nltk
+from nltk.chat.util import Chat, reflections
+def send():
+    send = "You -> "+e.get()
+    txt.insert(END, "n"+send)
+    user = e.get().lower()
+    if(user == "hello"):
+        txt.insert(END, "n" + "Bot -> Hi")
+    elif(user == "hi" or user == "hii" or user == "hiiii"):
+        txt.insert(END, "n" + "Bot -> Hello")
+    elif(e.get() == "how are you"):
+        txt.insert(END, "n" + "Bot -> fine! and you")
+    elif(user == "fine" or user == "i am good" or user == "i am doing good"):
+        txt.insert(END, "n" + "Bot -> Great! how can I help you.")
+    else:
+        txt.insert(END, "n" + "Bot -> Sorry! I dind't got you")
+    e.delete(0, END)
+
+pairs = [
+    [
+        r"my name is (.*)",
+        ["Hello %1, How are you today ?",]
+    ],
+    [
+        r"hi|hey|hello",
+        ["Hello", "Hey there",]
+    ], 
+    [
+        r"what is your name ?",
+        ["I am a jarvis created by harshit. you can call me crazy!",]
+    ],
+    [
+        r"how are you ?",
+        ["I'm doing goodnHow about You ?",]
+    ],
+    [
+        r"sorry (.*)",
+        ["Its alright","Its OK, never mind",]
+    ],
+    [
+        r"I am fine",
+        ["Great to hear that, How can I help you?",]
+    ],
+    [
+        r"i'm (.*) doing good",
+        ["Nice to hear that","How can I help you?:)",]
+    ],
+    [
+        r"(.*) age?",
+        ["I'm a computer program dudenSeriously you are asking me this?",]
+    ],
+    [
+        r"what (.*) want ?",
+        ["Make me an offer I can't refuse",]
+    ],
+    [
+        r"(.*) created ?",
+        ["harshit created me using Python's NLTK library ","top secret ;)",]
+    ],
+    [
+        r"(.*) (location|city) ?",
+        ['Indore, Madhya Pradesh',]
+    ],
+    [
+        r"how is weather in (.*)?",
+        ["Weather in %1 is awesome like always","Too hot man here in %1","Too cold man here in %1","Never even heard about %1"]
+    ],
+    [
+        r"i work in (.*)?",
+        ["%1 is an Amazing company, I have heard about it. But they are in huge loss these days.",]
+    ],
+    [
+        r"(.*)raining in (.*)",
+        ["No rain since last week here in %2","Damn its raining too much here in %2"]
+    ],
+    [
+        r"how (.*) health(.*)",
+        ["I'm a computer program, so I'm always healthy ",]
+    ],
+    [
+        r"(.*) (sports|game) ?",
+        ["I'm a very big fan of Football",]
+    ],
+    [
+        r"who (.*) sportsperson ?",
+        ["Messy","Ronaldo","Roony"]
+    ],
+    [
+        r"who (.*) (moviestar|actor)?",
+        ["Brad Pitt"]
+    ],
+    [
+        r"i am looking for online guides and courses to learn data science, can you suggest?",
+        ["Crazy_Tech has many great articles with each step explanation along with code, you can explore"]
+    ],
+    [
+        r"okay bye",
+        ["BBye take care. See you soon :) ","It was nice talking to you. See you soon :)"]
+    ],
+]
+def chat():
+    print("Hi! I am a jarvis  created by harshit for your service")
+    chat = Chat(pairs, reflections)
+    chat.converse()
+#initiate the conversation
+if __name__ == "__main__":
+    chat()
+
